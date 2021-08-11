58050fdb78145ebdb66e775e411a312dfbfc5b96
flowerYoungg <khy1896@gmail.com>
2021-08-12 08:37:28 +0900

    Aug12

--- react-basic/habit-tracker/src/components/navbar.jsx
+++ react-basic/habit-tracker/src/components/navbar.jsx
@@ -3,11 +3,11 @@ import React, { PureComponent } from "react";
 class Navbar extends PureComponent {
   render() {
     return (
-      <div className="navbar">
+      <header className="navbar">
         <i className="navbar-logo fas fa-leaf"></i>
         <span>Habit Tracker</span>
         <span className="navbar-count">{this.props.totalCount}</span>
-      </div>
+      </header>
     );
   }
 }
