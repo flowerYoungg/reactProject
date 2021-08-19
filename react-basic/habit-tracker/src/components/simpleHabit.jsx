b8d0b4a05e5c807ffb91ca9cbe35b497aa87066c
flowerYoungg <khy1896@gmail.com>
2021-08-20 08:36:32 +0900

    Aug20

--- react-basic/habit-tracker/src/components/simpleHabit.jsx
+++ react-basic/habit-tracker/src/components/simpleHabit.jsx
@@ -1,12 +1,19 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 
 const SimpleHabit = (props) => {
+  //setCount : count를 업데이트함
+  //useState : React Hook 에서 제공하는 API, 메모리에 값을 저장하고 있음
   const [count, setCount] = useState(0);
+  //useRef : 한 번 만들고 재사용
   const spanRef = useRef();
 
   const handleIncrement = useCallback(() => {
     setCount(count + 1);
   });
+  //useEffect : component가 처음 마운트되었을 때나 업데이트(count가 변경되면) 될 때마다 호출
+  useEffect(() => {
+    console.log(`mounted & updated : ${count}`);
+  }, [count]);
 
   return (
     <li className="habit">
