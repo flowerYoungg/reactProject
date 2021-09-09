11e5b28c4931c279bf9fbba1fd7e992516c9004e
flowerYoungg <khy1896@gmail.com>
2021-09-10 08:41:15 +0900

    Sep10

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello :D</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAogxapvRCPL5zKPZ6due5TiY-CqR_skZU",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
-//postman
-//start at 8.1 유튜브 프로젝트 시작💨
+//start at 8.1
