f9ee6c3a7884cb1e69ee5a68bd9a015ffe6ae28f
flowerYoungg <khy1896@gmail.com>
2022-01-10 16:52:08 +0900

    Jan10 third

---
+++ react-basic/router/src/components/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = (props) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1>HOME</h1>
+      <button
+        onClick={() => {
+          navigate("/profile");
+        }}
+      >
+        Go to Profile
+      </button>
+    </>
+  );
+};
+export default Home;
