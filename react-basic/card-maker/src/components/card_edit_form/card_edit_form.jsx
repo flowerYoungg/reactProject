79391c5a921b64228c90f4c507c496df491b6b45
flowerYoungg <khy1896@gmail.com>
2022-01-16 18:12:30 +0900

    Jan16

--- react-basic/card-maker/src/components/card_edit_form/card_edit_form.jsx
+++ react-basic/card-maker/src/components/card_edit_form/card_edit_form.jsx
@@ -1,6 +1,55 @@
 import React from "react";
+import Button from "../button/button";
 import styles from "./card_edit_form.module.css";
+import ImageFileInput from "../image_file_input/image_file_input";
 
-const CardEditForm = ({ card }) => <h1>{card.id}</h1>;
+const CardEditForm = ({ card }) => {
+  const { name, company, title, email, message, theme, fileName, fileURL } =
+    card;
+
+  const onSubmit = () => {};
+  return (
+    <form className={styles.form}>
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        value={name}
+      ></input>
+      <input
+        className={styles.input}
+        type="text"
+        name="company"
+        value={company}
+      ></input>
+      <select className={styles.select} name="theme" value={theme}>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="colorful">Colorful</option>
+      </select>
+      <input
+        className={styles.input}
+        type="text"
+        name="title"
+        value={title}
+      ></input>
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        value={email}
+      ></input>
+      <textarea
+        className={styles.textarea}
+        name="message"
+        value={message}
+      ></textarea>
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+      </div>
+      <Button name="Delete" onClick={onSubmit}></Button>
+    </form>
+  );
+};
 
 export default CardEditForm;
